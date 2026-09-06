4f8c618701f6f54d57bccc52e8286214d8103721
joppe <jopjeknopje@proton.me>
2026-09-06 14:37:57 +0200

    setup lsp

---
+++ .nvim.lua
@@ -0,0 +1,13 @@
+require("lspconfig").rust_analyzer.setup({
+settings = {
+  ["rust-analyzer"] = {
+			check = {
+				allTargets = false
+			},
+			cargo = {
+				target = "thumbv7m-none-eabi"
+			}
+		}
+}
+
+})
